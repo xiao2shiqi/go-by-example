1bdbc80519fe2a3aa947b1c6121cfa2b80f2e12a
phoenix-xiao <xiaobin.phoenix@gmail.com>
2021-02-25 21:28:46 +0800

    values go

---
+++ values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// string 字符串拼接
+	fmt.Println("go" + "lang")
+	// int 简单运算
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	// bool 布尔表达式
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
